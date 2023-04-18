3b85d506f7006b40e244da020df4bbc5ef2f23f4
Paulo Gosik M. Moita <126024627+paulogosik@users.noreply.github.com>
2023-04-18 17:59:19 -0300

    Create Teste Login

---
+++ Teste Login/AreaDeTestes.py
@@ -0,0 +1,16 @@
+# COLOCANDO COR EM TEXTO
+        # class color:
+        #    p = '\033[95m'
+        #    c = '\033[96m'
+        #    dc = '\033[36m'
+        #    bl = '\033[94m'
+        #    g = '\033[92m'
+        #    y = '\033[93m'
+        #    r = '\033[91m'
+        #    bo = '\033[1m'
+        #    und = '\033[4m'
+        #    end = '\033[0m'
+        #    i = '\x1B[3m'
+
+        # im = (f"{color.i}{color.c} Teste {color.end}")
+        # print(f"The output is: {im}")
---
+++ Teste Login/TesteLogin.py
@@ -0,0 +1,21 @@
+# Criação de funções
+def CriarConta():
+    name = input("=> Qual o seu nome? ")
+
+# Classe para cores
+class color:
+   p = '\033[95m'; c = '\033[96m'
+   dc = '\033[36m'; bl = '\033[94m'
+   g = '\033[92m'; y = '\033[93m'
+   r = '\033[91m'; bo = '\033[1m'
+   und = '\033[4m'; end = '\033[0m'
+   i = '\x1B[3m'
+   
+
+# Inicialização - Menu Inicial
+print(f"==[ Bem-vindo à {color.i}{color.c} IM!{color.end}. Como posso te ajudar? ]==\n"
+      f"    [1] Login\n"
+      f"    [2] Criar conta")
+opc = input("=> ")
+
+print('')

2d269cd7b53066c1955cb4b20d087a6a37a8626e
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-04-18 21:28:29 -0300

    Update TesteLogin.py

--- Teste Login/TesteLogin.py
+++ Teste Login/TesteLogin.py
@@ -1,6 +1,8 @@
 # Criação de funções
 def CriarConta():
-    name = input("=> Qual o seu nome? ")
+    name = input("=> Informe o seu nome: ")
+    name = name.title()
+    user = input("=> Informe o seu user")
 
 # Classe para cores
 class color:
@@ -9,13 +11,20 @@ class color:
    g = '\033[92m'; y = '\033[93m'
    r = '\033[91m'; bo = '\033[1m'
    und = '\033[4m'; end = '\033[0m'
-   i = '\x1B[3m'
+   i = '\x1B[3m';  w = '\033[37m'
+   
+class bg:
+    bl = '\033[40m'; r = '\033[41m'
+    g = '\033[42m'; y = '\033[43m'
+    bl = '\033[44m'; m = '\033[45m'
+    c = '\033[46m'; w = '\033[47m'
    
 
 # Inicialização - Menu Inicial
-print(f"==[ Bem-vindo à {color.i}{color.c}IM!{color.end}. Como posso te ajudar? ]==\n"
+print(f"{color.w}==[ Bem-vindo à {color.i}{color.c}IM!{color.end}. Como posso te ajudar? ]==\n"
       f"    [1] Login\n"
-      f"    [2] Criar conta")
+      f"    [2] Criar conta\n"
+      f"    {color.bo}{color.r}[3] Sair{color.end}")
 opc = input("=> ")
 
 print('')

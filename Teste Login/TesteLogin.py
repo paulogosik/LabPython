a14747c464ff6ad9dafedbbc3c7340dcfb727735
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-04-24 11:15:28 -0300

    Update TesteLogin.py

--- Teste Login/TesteLogin.py
+++ Teste Login/TesteLogin.py
@@ -1,5 +1,6 @@
 # Importações ---------------------------------
 import os
+import re
 # Classe para cores ---------------------------------
 class color:
    p = '\033[95m'; c = '\033[96m'
@@ -19,6 +20,28 @@ class bg:
 # Criação de funções --------------------------------
 def Clear():
     os.system('cls' if os.name == 'nt' else 'clear')
+    
+def VerificarSenha(password):
+        def Password():
+                senha = input("=> Informe sua senha: ")
+                return senha
+        
+        senha = Password()
+        while senha != password:
+                print(f"=> Senha incorreta! Deseja continuar?\n"
+                f"         {color.g}[1] Continuar{color.end}\n"
+                f"         {color.bo}{color.r}[2] Sair{color.end}")
+                continuar = int(input("=> "))
+                if continuar == 1:
+                        Clear()
+                        senha = Password()
+                elif continuar == 2:
+                        Clear()
+                        break
+        if senha == password:
+            Clear()
+            print(f"{color.g}=> Seja bem-vindo!{color.end}{color.bo} {name}{color.end}")
+
 def verificarUser():
     def Login():
         arquivo = open("//10.8.0.37/usuarios$/109103/Meus Documentos/testes/LabPython/Teste Login/database.txt", "r", encoding="utf8")
@@ -50,10 +73,44 @@ def verificarUser():
     return conta[0], conta[1], conta[2]              
 
 def CriarConta():
-    print("hello, world")
-
-def EntrarConta():
-    print("hello, world")
+        def UsersCriados():
+                arquivo = open("//10.8.0.37/usuarios$/109103/Meus Documentos/testes/LabPython/Teste Login/database.txt", "r", encoding="utf8")
+                conteudo = arquivo.readlines()
+                usersCriados = []
+                for line in conteudo:
+                        valores = line.split(";")
+                        usersCriados.append(valores[1])
+                return usersCriados
+        usersCriados = UsersCriados()
+        name = input("=> Informe o seu nome: ")
+        
+        def User():
+            user = input("=> Informe o user desejado: ")
+            return user
+        
+        user = User()
+        user = user.lower()
+        while user.lower() in usersCriados:
+            print(f"=> Usuário já está em uso! Deseja continuar?\n"
+            f"         {color.g}[1] Continuar{color.end}\n"
+            f"         {color.bo}{color.r}[2] Sair{color.end}")
+            continuar = int(input("=> "))
+            if continuar == 1:
+                    Clear()
+                    user = User()
+            elif continuar == 2:
+                    Clear()
+                    break
+                
+        password = int(input("=> Informe a sua senha (somente números): "))
+        password2 = int(input("=> Repita a sua senha: "))          
+        while password != password2:
+                Clear()
+                print(f"{color.r}=> Senhas diferentes!{color.end}")
+                password = int(input("=> Informe a sua senha (somente números): "))
+                password2 = int(input("=> Repita a sua senha: "))
+        user = user.lower()
+        return name, user, password
 
 def BuscarPasswords():
     passwords = []
@@ -99,6 +156,7 @@ opc = int(input("=> "))
 
 opcoes = [1, 2, 3]
 while opc not in opcoes:
+    Clear()
     print(f"{color.r}=> Opção Inválida!{color.end} Esolha uma das opções abaixo:\n"
           f"    {color.g}[1] Login\n"
           f"    [2] Criar conta\n{color.end}"
@@ -109,12 +167,18 @@ if opc == 1:
     Clear()
     name, user, password = verificarUser()
     Clear()
-    print(f"{color.g}=> Usuário encontrado!{color.end} Seja bem-vindo, {name}.")
+    print(f"{color.g}=> Usuário encontrado!{color.end}")
+    password = re.sub('[\n]', '', password)
+    VerificarSenha(password)
     
 elif opc == 2:
     Clear()
-    print("Opção 2")
+    name, user, password = CriarConta()
+    conta = [name, user, password]
+    print(conta)
+    contaF = f"{name};{user};{password}"
+    print(contaF)
     
 elif opc == 3:
     Clear()
-    print("Opção 3")
+    breakpoint

101e8f417afad083de2d80ad7b4f436885a531e0
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-08-22 17:46:09 -0300

    Update GeneratingPassword.py

--- PasswordGenerator/GeneratingPassword.py
+++ PasswordGenerator/GeneratingPassword.py
@@ -2,7 +2,7 @@
 import random
 
 # Characters tuples ---------------------------
-especialChar = ["@", "!", "#", "$", "%", "*", "(", ")", "_"]
+especialChar = ["@", "!", "$", "%", "*", "(", ")", "_"]
 upperChar = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 lowerChar = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -12,7 +12,6 @@ numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 def generate_weak_password(size):
     password = []
     i = 0
-    
     while i < size:
         option = random.randint(0,1)
         if option == 0:
@@ -28,7 +27,6 @@ def generate_weak_password(size):
 def generate_medium_password(size):
     password = []
     i = 0
-    
     while i < size:
         if i == 0:
             password.append(random.choice(numChar))
@@ -49,7 +47,6 @@ def generate_medium_password(size):
 def generate_strong_password(size):
     password = []
     i = 0
-    
     while i < size:
         if i == 0:
             password.append(random.choice(especialChar))

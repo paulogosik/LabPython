f42bedda67ba71043e536c74e55e588bd18c1cec
Paulo Gosik M. Moita <paulogosikmoita@gmail.com>
2023-08-22 17:44:45 -0300

    Update main.py

--- PasswordGenerator/main.py
+++ PasswordGenerator/main.py
@@ -1,6 +1,8 @@
 # Imports ---------------------------
 import GeneratingPassword # type: ignore
 import os
+from tqdm import tqdm
+import time
 
 # Class ---------------------------
 class color:
@@ -19,4 +21,34 @@ def clear():
 clear()
 size = int(input(f"{color.i}=> Inform the size of the password: {color.end}"))
 
-GeneratingPassword.generate_password(size)
+while size < 6 or size > 24:
+    clear()
+    size = int(input(f"{color.i}{color.r}The password needs to be bigger than 6 and smaller than 24.{color.end}\n{color.i}=> Inform the size of the password: {color.end}"))
+
+weakPassword = GeneratingPassword.generate_weak_password(size)
+mediumPassword = GeneratingPassword.generate_medium_password(size)
+strongPassword = GeneratingPassword.generate_strong_password(size)
+
+clear()
+result = 0
+for i in tqdm(range(3), ncols=80, unit="Mb"):
+    time.sleep(1)
+    result += i
+
+clear()
+weak = f"{color.r}Weak{color.end}"
+medium = f"{color.y}Medium{color.end}"
+strong = f"{color.g}Strong{color.end}"
+
+print(f"{color.w}-" * 40)
+print(f"{color.i}{color.w}{'PASSWORDS':^40}{color.end}")
+print(f"{color.w}-" * 40)
+
+print(f"{weak:.<25}", end='')
+print(f"{weakPassword:.>24}")
+print(f"{medium:.<25}", end='')
+print(f"{mediumPassword:.>24}")
+print(f"{strong:.<25}", end='')
+print(f"{strongPassword:.>24}")
+
+print(f"{color.w}-" * 40)
